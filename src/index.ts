55d674c88e2331c494af95810a3309ba01012c60
kadir belen <kadir@bordo.io>
2023-09-14 22:11:52 +0300

    feat: add pino logger

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,17 @@ import 'express-async-errors';
 
 import express from 'express';
 
+import { logger, pinoLogger } from './configs/logger.config';
 import { errorHandler } from './middlewares/error-handler.middleware';
 import orderRouter from './routers/order.router';
 import productRouter from './routers/product.router';
 import userRouter from './routers/user.router';
 
 const app = express();
+
 app.use(express.json());
+
+app.use(logger);
 
 app.use('/users', userRouter);
 app.use('/products', productRouter);
@@ -18,5 +22,5 @@ app.use('/orders', orderRouter);
 app.use(errorHandler);
 
 app.listen(3000 || process.env.PORT, () => {
-  console.info('application is running');
+  pinoLogger.info('application is running');
 });
